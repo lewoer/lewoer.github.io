de9c1bfb5b06732924e9ac3b5e2ec28629c7332f
lewoer <shile4408@126.com>
2018-07-16 22:11:15 +0800

    Site updated: 2018-07-16 22:11:03

--- js/src/schemes/pisces.js
+++ js/src/schemes/pisces.js
@@ -1,3 +1,3 @@
-// build time:Mon Jul 16 2018 22:06:40 GMT+0800 (中国标准时间)
+// build time:Mon Jul 16 2018 22:10:53 GMT+0800 (中国标准时间)
 $(document).ready(function(){var n=$(".header-inner");var r=$("#sidebar");var e=function(){return n.height()+CONFIG.sidebar.offset};var i=function(n){return r.css({"margin-top":n})};var a=window.matchMedia("(min-width: 991px)");i(e()).show();a.addListener(function(n){if(n.matches){i(e())}})});
 //rebuild by neat 
